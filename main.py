ab44f4c699f44bf69ab31c8c61dfaa13c843d10f
Edgar Dralov <edgar.dralov@gmail.com>
2023-11-30 20:58:06 +0300

    АшÐFirst API project

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get('/')
 def read():
     return "Hello world"
+
+@app.get('/name/{firstname}')
+def read(firstname: str, lastname: str = ''):
+    return f'Hello {firstname} {lastname}'
+
