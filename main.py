3a9dc2df5b4bb26e144fbb8ee6508ed3e460a72f
Edgar Dralov <edgar.dralov@gmail.com>
2023-12-06 17:16:11 +0300

    First API project

--- main.py
+++ main.py
@@ -1,34 +1,60 @@
-from typing import Optional
-from fastapi import FastAPI
-from pydantic import BaseModel
+#  Create FastAPI application in Docker with all requests (GET, RETRIEVE, UPDATE - PUT\PATCH, POST, DELETE)
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import Dict
+
+app = FastAPI(
+    title='First API Project'
+)
+
 
 class User(BaseModel):
-    name: Optional[str]
+    id: int
+    name: str
     age: int
+    email: str
     is_admin: bool
 
-@app.get('/')
-def read():
-    return "Hello world"
 
-@app.get('/name/{firstname}')
-def read(firstname: str, lastname: str = ''):
-    return f'Hello {firstname} {lastname}'
+db_users: Dict[int, User] = {
+    1: User(id=1, name="Petr", age=23, email="petr@gmail.com", is_admin=True),
+    2: User(id=2, name="Nata", age=23, email="Nata@hmail.vom", is_admin=False)
+}
 
-@app.get('/user')
-def update_user():
-    return 'user'
+# GET ALL users
+@app.get("/users/", response_model=Dict[int, User])
+def read_user():
+    return db_users
 
-@app.get('/user/{user_id}')
-def update_user(user_id: int):
-    return user_id
+# Get request users ID
+@app.get("/users/{users_id}", response_model=User)
+def read_user(user_id: int):
+    user = db_users.get(user_id)
+    if user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
 
-@app.put('/user/{user_id}')
-def update_user(user_id: int, user: User):
-    return {'name': user.name, 'age': user.age, 'is_admin': user.is_admin, 'id': user_id}
 
-@app.post('/user')
+# POST request create user
+@app.post("/users/", response_model=User)
 def create_user(user: User):
-    return user.__dict__
+    db_users[user.id] = user
+    return user
+
+
+# PUT request for update user
+@app.put("/user/{user_id}", response_model=User)
+def update_user(user_id: int, user: User):
+    if user_id not in db_users:
+        raise HTTPException(status_code=404, detail="User not found")
+    db_users[user_id] = user
+
+
+# DELETE request
+@app.delete("/users/{user_id}")
+def delete_user(user_id: int):
+    if user_id not in db_users:
+        raise HTTPException(status_code=404, detail="User not found")
+    del db_users[user_id]
+    return {'message': 'User delete successful'}
