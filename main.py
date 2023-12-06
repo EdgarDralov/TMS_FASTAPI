32a130318fd0f1ebf5131c1700ad2baaeb950b2a
Edgar Dralov <edgar.dralov@gmail.com>
2023-12-06 12:30:17 +0300

    ÐFirst API project

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class User(BaseModel):
+    name: str
+    age: int
+    is_admin: bool
 
 @app.get('/')
 def read():
@@ -10,3 +16,14 @@ def read():
 def read(firstname: str, lastname: str = ''):
     return f'Hello {firstname} {lastname}'
 
+@app.get('/user')
+def update_user():
+    return 'user'
+
+@app.get('/user/{user_id}')
+def update_user(user_id: int):
+    return user_id
+
+@app.put('/user/{user_id}')
+def update_user(user_id: int, user: User):
+    return {'name': user.name, 'age': user.age, 'is_admin': user.is_admin, 'id': user_id}
